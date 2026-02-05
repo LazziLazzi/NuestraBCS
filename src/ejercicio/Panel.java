94d3b7d4707b7bee92f45a3723e429d61a2d708a
Ceyla C. <cech_23@alu.uabcs.mx>
2026-02-05 13:51:32 -0700

    First commit

---
+++ src/ejercicio/Panel.java
@@ -0,0 +1,10 @@
+package ejercicio;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+	public Panel() {
+		setBackground(new Color(0, 0, 0));
+	}
+}
---
+++ src/ejercicio/Ventana.java
@@ -0,0 +1,30 @@
+package ejercicio;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+	public Ventana() {
+		setSize(500, 500);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setLocation(500, 200);
+		//setBounds(getBounds());
+		
+		setResizable(false);
+		setTitle("Silencio Gil");
+		setLocationRelativeTo(null);
+		
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Image icono = tk.getImage("C:\\Users\\lalac\\Documents\\Universidad\\4to semestre\\Programación 3\\Prog Unidad 1\\ejercicioDeEjemplo\\src\\images\\image.jpg");
+		setIconImage(icono);
+		
+		Panel panelito = new Panel();
+		add(panelito);
+		
+		setVisible(true);
+		validate();
+		repaint();
+	}
+}
