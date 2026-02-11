65d6cf23eed0d03286008db8d31de14db5a088d5
Diego Yepiz Langarica <Diyela21@gmail.com>
2026-02-10 18:15:48 -0700

    Se acomodo por clases los componentes para organizar mejor

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -15,64 +15,88 @@ import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
 public class LoginView extends JPanel {
+	
+	//Atributos
+	private JTextField Correo;
+	private JPasswordField Contrasenia;
+	private JButton Acceder;
+	
+	//Colores
+	private Color Verde = new Color(56,142,60);
+	private Color Amarillito = new Color(255, 255, 204);
+	private Font Titulo = new Font("Arial", Font.BOLD, 30);
+	private Font General = new Font("Arial", Font.BOLD, 16);
+	
+	//Contructor
 	public LoginView() {
-		
-		Font fuente = new Font("Arial", Font.BOLD, 20);
-		Color verde = new Color(56, 142, 60);
-		Color amarillito = new Color(255,255,204);
-		
+		panel();
+		etiquetas();
+		campo();
+		boton();
+	}
+	
+	//Metodos
+	
+	private void panel() {
 		setLayout(null);
-		setBackground(amarillito);
-		
-		//Título y sus componentes
-		JLabel titulo = new JLabel("NuestraBCS"); 
-		titulo.setFont(new Font("Arial", Font.BOLD, 30));
+		setBackground(Amarillito);
+	}
+	
+	private void etiquetas() {
+		//Titulo
+		JLabel titulo = new JLabel("NuestraBCS");
+		titulo.setFont(Titulo);
 		titulo.setBounds(160,20,300,100);
-		titulo.setForeground(verde);
+		titulo.setForeground(Verde);
 		add(titulo);
-		
-		//Texto para ingresar correo
+		//Correo
 		JLabel ingreseCorreo = new JLabel("Ingrese su correo electrónico:"); 
-		ingreseCorreo.setFont(new Font("Arial", Font.BOLD, 16));
+		ingreseCorreo.setFont(General);
 		ingreseCorreo.setBounds(115,60,300,100);
 		ingreseCorreo.setForeground(Color.BLACK);
 		add(ingreseCorreo);
-		
-		//Texto para ingresar contraseña
+		//Contrasenia
 		JLabel ingreseContrasenia = new JLabel("Ingrese su contraseña:"); 
-		ingreseContrasenia.setFont(new Font("Arial", Font.BOLD, 16));
+		ingreseContrasenia.setFont(General);
 		ingreseContrasenia.setBounds(115,140,300,100);
 		ingreseContrasenia.setForeground(Color.BLACK);
 		add(ingreseContrasenia);
-		
-		//Caja de texto para ingresar correo
+	}
+	
+	private void campo() {
+		//Cmpo de correo
 		JTextField correo = new JTextField();
 		correo.setFont(new Font("Arial", Font.PLAIN, 18));
 		correo.setBounds(110,130,280,40);
 		add(correo);
-		
-		//Caja de texto para ingresar contraseña
+		//Campo de contrasenia
 		JPasswordField contrasenia = new JPasswordField();
 		contrasenia.setFont(new Font("Arial", Font.PLAIN, 25));
 		contrasenia.setBounds(110,210,280,40);
 		add(contrasenia);
-		
+	}
+	
+	private void boton() {
 		//Botón de acceso y sus componentes
 		JButton boton = new JButton("Acceder"); //creación del botón
 		boton.setBounds(150,300,200,40); //coordenadas del botón
-		boton.setBackground(verde); //color de fondo del botón
+		boton.setBackground(Verde); //color de fondo del botón
 		boton.setForeground(Color.WHITE); //color de la fuente de texto del botón
 		boton.setToolTipText("Da click para iniciar sesión");
-		boton.setFont(fuente); //ajuste de la fuente del boton
+		boton.setFont(General); //ajuste de la fuente del boton
 		
+		add(boton);
+	}
+	
+	private void cargarIcono() {
 		try {
 			Image icono = ImageIO.read(getClass().getResource("../images/flecha.png")); //lee la imagen
-			icono = icono.getScaledInstance(20, 25, Image.SCALE_SMOOTH); //escala la imagen/cambia el tamaño
-			boton.setIcon(new ImageIcon(icono));
+			if(icono != null) {
+				icono = icono.getScaledInstance(20, 25, Image.SCALE_SMOOTH); //escala la imagen/cambia el tamaño
+				Acceder.setIcon(new ImageIcon(icono));
+			}
 		}catch(Exception ex) {
 			System.out.println("No está la imagen del icono.");
 		}
-		
-		add(boton);
 	}
 }
