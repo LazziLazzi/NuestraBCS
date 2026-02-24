4759809cea88dd758d738dee8b61b8834abeeb99
Ceyla C. <cech_23@alu.uabcs.mx>
2026-02-24 09:57:07 -0700

    Agregué un paquete para las fuentes y cambié la fuente del título

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -18,6 +18,8 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
+import utils.AppFont;
+
 public class LoginView extends JPanel {
 	
 	//Atributos
@@ -61,7 +63,7 @@ public class LoginView extends JPanel {
 	private void etiquetas() {
 		// Texto del título
 		JLabel titulo = new JLabel("NuestraBCS");
-		titulo.setFont(fuenteTitulo);
+		titulo.setFont(AppFont.title());
 		titulo.setForeground(verde);
 		titulo.setAlignmentX(0.5f); 
 		add(titulo);
