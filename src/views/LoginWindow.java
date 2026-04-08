a6c15613c6286322451b4649d838ab657ce0d3f4
Ceyla C. <cech_23@alu.uabcs.mx>
2026-04-08 09:40:26 -0700

    paquete de controllers y models agregados

--- src/views/LoginWindow.java
+++ src/views/LoginWindow.java
@@ -4,29 +4,26 @@ import javax.swing.JFrame;
 import java.awt.Dimension;
 
 public class LoginWindow extends JFrame{
-		public LoginWindow() {
-			setTitle("NuestraBCS - Iniciar Sesión");
-	        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-	       
-	        LoginView loginPanel = new LoginView();
-	        add(loginPanel);
-	        
-	        setSize(400, 500); 
-	        setLocationRelativeTo(null);
-	        
-	        addWindowListener(new java.awt.event.WindowAdapter() {
-	            @Override
-	            public void windowClosing(java.awt.event.WindowEvent e) {
-	                int option = javax.swing.JOptionPane.showConfirmDialog(null, 
-	                    "¿Desea salir de NuestraBCS?", "Salir", 
-	                    javax.swing.JOptionPane.YES_NO_OPTION);
-	                if (option == javax.swing.JOptionPane.YES_OPTION) {
-	                    System.exit(0);
-	                }
-	            }
-	        });
-	        
-		}
-		
-		
+	public LoginWindow() {
+		setTitle("NuestraBCS - Iniciar Sesión");
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+       
+        LoginView loginPanel = new LoginView();
+        add(loginPanel);
+        
+        setSize(400, 500); 
+        setLocationRelativeTo(null);
+        
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent e) {
+                int option = javax.swing.JOptionPane.showConfirmDialog(null, 
+                    "¿Desea salir de NuestraBCS?", "Salir", 
+                    javax.swing.JOptionPane.YES_NO_OPTION);
+                if (option == javax.swing.JOptionPane.YES_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });        
+	}		
 }
