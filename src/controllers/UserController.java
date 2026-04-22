fda7555d8ad29f1419c09b8bd73969ca3ad6f3b7
Diego Yepiz Langarica <Diyela21@gmail.com>
2026-04-22 08:46:39 -0700

    Ya se ve la tabla

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -1,10 +1,14 @@
 package controllers;
 
 import repository.UserRepository;
+import models.User;
 import models.UserTableModel;
 import views.UserFormDialog;
 import views.UsersView;
 import javax.swing.SwingUtilities;
+
+import java.util.List;
+
 import javax.swing.JFrame;
 
 public class UserController {
@@ -16,6 +20,8 @@ public class UserController {
     public UserController(UsersView view) {
         this.view = view;
         this.repo = new UserRepository();
+        
+        chargeTable();
      
         //El boton agregar abre el userformdialog
         view.getBtnAdd().addActionListener(e -> {
@@ -35,4 +41,17 @@ public class UserController {
             System.out.println("Borrar dislaik");
         });
     }
+    
+    public void chargeTable() {	
+    		try {
+    			List<User> users = repo.getUsers();
+    			model = new UserTableModel(users);
+    			view.setTableModel(model);
+    		}catch(Exception ex){
+    			System.out.println("Error al cargar el CSV" + ex.getMessage());
+    		}
+    	
+    }
+    
+    
 }
